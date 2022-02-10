9afc6445951b31262cb56fdc1116804d83c8a6c8
Chad Linthicum <portableanswers@gmail.com>
2022-02-10 18:39:45 -0500

    Seconds hand functioning off of true value seconds

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext('2d');
 
-const time = new Date();
-seconds = time.getSeconds();
+
 let handsX = 200; //start point of hands
 let handsY = 200; //start point of hands
-  
-drawClockFace(); 
 
-drawSecondsHand();
-
-// setInterval(function() {
-//   drawSecondsHand();
-// }, 500);
+setInterval(function() {
+  drawClockFace(); 
+  drawSecondsHand();
+}), 500;
 
 function drawClockFace() {
   ctx.beginPath();
@@ -23,43 +19,11 @@ function drawClockFace() {
 }
 
 function drawSecondsHand() {
+  const seconds = new Date().getSeconds();
   ctx.translate(200,200);
-
-
-  
-
-  if (seconds == 1) {
-    degrees = 6
-    ctx.rotate(Math.PI / 180 * degrees);
-  }  
-
-  if (seconds == 2) {
-    degrees = 12
-    ctx.rotate(Math.PI / 180 * degrees);
-  }  
-
+  degrees = seconds * 6;
+  ctx.rotate(Math.PI / 180 * degrees);
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,2,-175);
+  ctx.resetTransform(); 
 }
-
-
-
-
-
-// function secondHandStartPos() {  
-//   ctx.translate(handsX, handsY); 
-//   ctx.rotate( (Math.PI / 180 * (seconds*6))); 
-//   // ctx.translate(-handsX, -handsY);
-// }
-
-// function secondHandMovement() {
-//   let L = -175; //sets the seconds hand to 12 position
-//   let W = 2;
-//   // ctx.translate(handsX, handsY); 
-//   ctx.rotate( (Math.PI / 180 * 6));
-//   // ctx.translate(-handsX, -handsY); 
-//   ctx.fillStyle = 'black';
-//   ctx.fillRect(handsX, handsY, W, L);
-//   let seconds = (new Date()).getSeconds();
-//   console.log("seconds = " + seconds);
-// }

8443cc7d2c13de936b50a8bca395faa2be7f5792
Chad Linthicum <portableanswers@gmail.com>
2022-02-11 14:43:06 -0500

    Added numbers and background to clock face.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext('2d');
 
+const moon = document.getElementById('moon');
+
 setInterval(function() {
-  clockFace(185, '#D85151'); 
+  
+  ctx.drawImage(moon, 0, 0, canvas.width, canvas.height);
   hoursHand();
   minutesHand();
   secondsHand();
-  clockFace(13, 'black'); 
+  clockFace(13, 'black');
+   
 }), 500;
 
 function clockFace(radius, color) {
   ctx.beginPath();
-  ctx.arc(200, 200, radius, 0, 2 * Math.PI);
+  ctx.arc(canvas.width/2,canvas.height/2, radius, 0, 2 * Math.PI);
   ctx.lineWidth=7;
   ctx.stroke(); 
   ctx.fillStyle = color;
@@ -20,7 +24,7 @@ function clockFace(radius, color) {
 
 function secondsHand() {
   const seconds = new Date().getSeconds();
-  ctx.translate(200,200);
+  ctx.translate(canvas.width/2,canvas.height/2);
   degrees = seconds * 6;
   ctx.rotate(Math.PI / 180 * degrees);
   ctx.fillStyle = "black";
@@ -30,21 +34,23 @@ function secondsHand() {
 
 function minutesHand() {
   const minutes = new Date().getMinutes();
-  ctx.translate(200,200);
+  ctx.translate(canvas.width/2,canvas.height/2);
   degrees = minutes * 6;
   ctx.rotate(Math.PI / 180 * degrees);
   ctx.fillStyle = "black";
-  fillRectCentered(0, 0, 10, -175);
+  fillRectCentered(0, 0, 10, -145);
   ctx.resetTransform();  
 }
 
 function hoursHand() {
   const hours = new Date().getHours();
-  ctx.translate(200,200);
+  ctx.translate(canvas.width/2,canvas.height/2);
+  
+
   degrees = hours * 30;
   ctx.rotate(Math.PI / 180 * degrees);
   ctx.fillStyle = "black";
-  fillRectCentered(0,0,15,-125);
+  fillRectCentered(0,0,15,-100);
   ctx.resetTransform();  
 }
 
@@ -52,3 +58,4 @@ function hoursHand() {
 function fillRectCentered(x, y, width, height) {
   ctx.fillRect(x - width / 2, y, width, height);
 }
+
